47a4c69eb29a3b280341fb443e9f419f0671906a
bkupie <bkupiec150@gmail.com>
2021-07-09 19:41:24 -0500

    first changes for pygame

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# This is a sample Python script.
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
 
+pygame.init()
+dis = pygame.display.set_mode((400, 300))
+pygame.display.update()
+pygame.display.set_caption('Snake game by Bart')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.quit()
+quit()
